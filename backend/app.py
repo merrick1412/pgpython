1194dbdcc0e4f8daae63766a50ec39a895fd683e
merrick1412 <120765132+merrick1412@users.noreply.github.com>
2025-05-06 20:36:19 -0400

    html changes

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  
 
 DB_CONFIG = {
     'dbname': 'hello_world', 
