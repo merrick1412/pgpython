ecf00d3bb2d13e4ce382a8d861b2d1b67928d3ce
merrick1412 <120765132+merrick1412@users.noreply.github.com>
2025-05-19 21:40:00 -0400

    updated server config

--- backend/app.py
+++ backend/app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 CORS(app)  
 
 DB_CONFIG = {
-    'dbname': 'hello_world', 
-    'user': 'student',
-    'password': 'rpstudent',
+    'dbname': 'merrick_server', 
+    'user': 'merrick',
+    'password': 'upset_stomach',
     'host': '34.125.49.223',  # or your database server's IP
     'port': 5432          # default PostgreSQL port
 }
